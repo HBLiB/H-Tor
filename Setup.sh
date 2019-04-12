600bd4425ed2c9e8ac11d3c6579a7f59a98ddc54
Halil Baysal <hbl@worldstream.nl>
2019-04-12 23:32:51 +0200

    1

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
-sudo apt install bridge-utils  docker.io docker-compose
+sudo apt install -y bridge-utils  docker.io docker-compose
 sudo brctl addbr ToR
-docker network create --attachable --gateway 2.2.2.1 --subnet 2.2.2.0/24 --opt com.docker.network.bridge.name=ToR ToR
+sudo docker network create --attachable --gateway 2.2.2.254 --subnet 2.2.2.0/24 --opt com.docker.network.bridge.name=ToR ToR
+cd Docker
+sudo docker-compose build
+sudo docker-compose up -d
